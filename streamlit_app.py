5cca14355c04f404186b17ca46653dd076643197
bcoolkarthik.snowflake <109313014+bcoolkarthik@users.noreply.github.com>
2022-07-25 18:07:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
+cffi == 1.15.0
+pyarrow==6.0.1
 streamlit.header('🥣 Breakfast Menu')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
@@ -26,8 +28,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 def get_fruit_load_list():
    with my_cnx.cursor() as my_cur:
      role= "accountadmin"
-     cffi == 1.15.0
-     pyarrow==6.0.1
      streamlit.text("Snoflake function being called!!!!!!!!!!")
      my_cur.execute("SELECT * from fruit_load_list")
 
