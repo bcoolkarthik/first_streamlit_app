202904c6c45f7c7dd508210707357bacc3975c46
bcoolkarthik.snowflake <109313014+bcoolkarthik@users.noreply.github.com>
2022-07-25 17:50:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,13 +21,14 @@ def get_fruityvice_data(this_fruit_choice):
 streamlit.header("Fruityvice Fruit Advice!")
 def get_fruit_load_list():
    with my_cnx.cursor() as my_cur:
-      my_cur.execute("select * from fruit_load_list")
+      my_cur.execute("SELECT * from fruit_load_list")
       role= "accountadmin"
    return my_cur.fetchall()
 
 if streamlit.button('Get fruit load list'):
         
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   role= "accountadmin"
    my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
 streamlit.stop()
